58d7bf2240d99481046220ee118b5b16a940fe52
Peter Pristas <pristas.peter@gmail.com>
2018-12-14 15:46:55 +0100

    Rewrite ssr-webpack-dev-server to use express instead of koa

--- packages/ssr-webpack-dev-server/index.ts
+++ packages/ssr-webpack-dev-server/index.ts
@@ -2,8 +2,27 @@ import * as path from 'path';
 import {runInThisContext} from 'vm';
 import * as webpack from 'webpack';
 import fetch, { Response } from 'node-fetch';
-import * as Koa from 'koa';
-import * as koaWebpack from 'koa-webpack';
+import * as express from 'express';
+import * as middleware from 'webpack-dev-middleware';
+import * as httpProxy from 'http-proxy';
+import * as http from 'http';
+import * as compression from 'compression';
+
+const sourceMap = new Map<string, Buffer>();
+
+require('source-map-support').install({
+    retrieveSourceMap: function(url: string) {
+        const map = sourceMap.get(url);
+
+        if (map) {
+            return {
+                url,
+                map: map.toString(),
+            };
+        }
+        return null;
+    }
+});
 
 export class AssetReadError extends Error {
     static code = 'ASSET_READ_ERROR';
@@ -37,48 +56,65 @@ export function watch(
     config: webpack.Configuration,
     host = '0.0.0.0',
     port = 5000,
-    webpackDevserverUrl = 'http://127.0.0.1:3000/',
+    webpackDevserverUrl = 'http://localhost:3000/',
 ) {
 
-    let lastEndTime: number;
-    let serverMiddleware: Koa.Middleware;
+    let lastEndTime: webpack.Stats['endTime'];
+    let serverMiddleware: express.RequestHandler;
+    
+    const app = express();
+    const proxy = httpProxy.createProxyServer({
+        target: webpackDevserverUrl,
+        ws: true,
+    });
 
-    return koaWebpack({
-        compiler: webpack(config) as any,
-        devMiddleware: {
-            logLevel: 'info',
-            serverSideRender: true,
-            stats: 'minimal',
-        } as any,
-        hotClient: false,
-    })
-        .then(middleware => {
-            const fs = middleware.devMiddleware.fileSystem;
-            const app = new Koa();
+    app.use(compression());
 
-            app.use(middleware);
-            app.use((context, next) => {
-                context.readAsset = (asset: string) => 
-                    fetch(`${webpackDevserverUrl}${asset}`).then(response => {
-                        if (response.ok) {
-                            if (asset.endsWith('.json')) {
-                                return response.json();
-                            }
+    app.use(middleware(webpack(config) as any, {
+        logLevel: 'info',
+        serverSideRender: true,
+        stats: 'minimal',
+    } as any));
 
-                            return response.text();
-                        }
-                        return Promise.reject(new AssetReadError(response))
-                    });
-
-                if (lastEndTime !== context.state.webpackStats.endTime) {
-                    serverMiddleware = runInThisContext(fs.readFileSync(outputFile(context.state.webpackStats))).default;
+    app.use((req, res, next) => {
+        const {fs, webpackStats} = res.locals as {fs: any, webpackStats: webpack.Stats};
+        
+        res.locals.readAsset = (asset: string) => 
+        fetch(`${webpackDevserverUrl}${asset}`).then(response => {
+            if (response.ok) {
+                if (asset.endsWith('.json')) {
+                    return response.json();
                 }
 
-                lastEndTime = context.state.webpackStats.endTime;
-                return serverMiddleware(context, next);
-            });
-
-            app.listen(port, host);
+                return response.text();
+            }
+            return Promise.reject(new AssetReadError(response))
         });
 
+        if (lastEndTime !== webpackStats.endTime) {
+            const filename = outputFile(webpackStats);
+            const mapFilename = `${filename}.map`;
+
+            if (fs.existsSync(mapFilename)) {
+                sourceMap.set(filename, fs.readFileSync(mapFilename));
+            }
+
+            serverMiddleware = runInThisContext(fs.readFileSync(filename), filename).default;
+        }
+
+        lastEndTime = webpackStats.endTime;
+        serverMiddleware(req, res, next);
+    });
+
+    app.use((req, res) => {
+        proxy.web(req, res);
+    });
+
+    const server = http.createServer(app)
+
+    server.on('upgrade', function (req, socket, head) {
+        proxy.ws(req, socket, head);
+    });
+
+    server.listen(port, host);
 }
