bdd437abbc24e67e5d50a0c6ca61af4623888e8c
Peter Pristas <pristas.peter@gmail.com>
2018-12-18 13:17:43 +0100

    Update ssr-server implementation

---
+++ packages/ssr-server/bin.ts
@@ -0,0 +1,39 @@
+import {ArgumentParser} from 'argparse';
+import * as path from 'path';
+import serve from './index';
+import {version} from './package.json';
+
+const parser = new ArgumentParser({
+    addHelp: true,
+    version,
+    prog: 'ssr-server',
+});
+
+parser.addArgument([ '-m', '--middleware' ], {
+    required: true,
+    help: 'middleware path',
+});
+
+parser.addArgument([ '-b', '--build-path' ], {
+    help: 'build path',
+    defaultValue: path.join(process.cwd(), 'build'),
+});
+
+parser.addArgument('--hostname', {
+    defaultValue: '0.0.0.0',
+    help: 'hostname'
+});
+
+parser.addArgument('--port', {
+    defaultValue: 5000,
+    type: 'int',
+    help: 'port'
+});
+
+const args = parser.parseArgs();
+const middlewarePath = path.isAbsolute(args.middleware) ? args.middleware : path.join(process.cwd(), args.middleware);
+
+serve(require(middlewarePath).default, args.buildPath, args.host, args.port)
+    .catch(err => {
+        throw(err)
+    });
