98b1170a649fb898860fdc1cb14399ff86e42f9c
Peter Pristas <pristas.peter@gmail.com>
2019-01-10 09:57:43 +0100

    Add support for options via file and webpack

--- packages/css-modules-typings-loader/index.js
+++ packages/css-modules-typings-loader/index.js
@@ -1,15 +1,29 @@
 const {basename} = require('path');
 const {readFile, writeFile} = require('fs');
 const {camelize} = require('humps');
+const {getOptions} = require('loader-utils');
+
+function getConfig(loader) {
+    const config = getOptions(loader); 
+
+    try {
+        Object.assign(config, require(path.join(process.cwd(), '.cssmodulestypings')));
+    } catch (err) {
+        // pass
+    }
+
+    return {
+       suffix: () => 'Styles',
+       ...config,
+    };
+};
 
 function capitalizeFirstLetter(s) {
     return s.charAt(0).toUpperCase() + s.slice(1);
 }
 
 function stripExtension(s) {
-    const name = s.split('.')[0];
-
-    return `${name}Styles`;
+    return s.split('.')[0];
 }
 
 function validateName(s) {
@@ -24,7 +38,7 @@ module.exports = function loader(webpackSource) {
     this.cacheable();
 
     const callback = this.async();
-
+    
     const end = (err) => {
         if (err) {
             callback(err);
@@ -32,10 +46,11 @@ module.exports = function loader(webpackSource) {
             callback(null, webpackSource);
         }
     };
-
+    
     const match = webpackSource.match(/exports\.locals.*=([^]*?);/);
-
+    
     if (match) {
+        const config = getConfig(this);
         const locals = [];
 
         match[1].split('\n').map(s => {
@@ -47,7 +62,8 @@ module.exports = function loader(webpackSource) {
         });
 
         if (locals.length) {
-            const name = validateName(capitalizeFirstLetter(camelize(stripExtension(basename(this.resource)))));
+            const suffix = config.suffix(this.resource);
+            const name = `${validateName(capitalizeFirstLetter(camelize(stripExtension(basename(this.resource)))))}${suffix}`;
 
             const output = `/* tslint:disable */
 // This file was automatically generated and should not be edited.
